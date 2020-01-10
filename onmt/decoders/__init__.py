c652512b95770422881e099756f57f28ca6e500a
KaijuML <clement.rebuffel@lip6.fr>
2020-01-10 16:11:30 +0100

    ONMT + dualattn model (without modified LSTM)

---
+++ onmt/decoders/__init__.py
@@ -0,0 +1,14 @@
+"""Module defining decoders."""
+from onmt.decoders.decoder import DecoderBase, InputFeedRNNDecoder, \
+    StdRNNDecoder
+from onmt.decoders.transformer import TransformerDecoder
+from onmt.decoders.cnn_decoder import CNNDecoder
+from onmt.decoders.structure_aware import StructureAwareDecoder
+
+
+str2dec = {"rnn": StdRNNDecoder, "ifrnn": InputFeedRNNDecoder,
+           "cnn": CNNDecoder, "transformer": TransformerDecoder,
+           "sarnn": StructureAwareDecoder}
+
+__all__ = ["DecoderBase", "TransformerDecoder", "StdRNNDecoder", "CNNDecoder",
+           "InputFeedRNNDecoder", "str2dec", "StructureAwareDecoder"]
