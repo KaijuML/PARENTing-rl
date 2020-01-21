d4a229dc354f65585094cc12f6ec26a9c2532913
KaijuML <clement.rebuffel@lip6.fr>
2020-01-21 10:29:14 +0100

    finished batching script for metrics

--- batch_metric.py
+++ batch_metric.py
@@ -44,3 +44,41 @@ if __name__ == "__main__":
     list_of_references = load_refs(args.dataset, args.setname)
     tables = load_tables(args.dataset, args.setname)
     print("TABLES and REFERENCES loaded")
+    
+    res_filename = os.path.join(folder, 'results.json')
+    if os.path.exists(res_filename):
+        with open(res_filename, encoding='utf8', mode='r') as resfile:
+            res_content = json.load(resfile)
+    else:
+        res_content = dict()
+        
+    filenames = [os.path.join(folder, fname)
+                 for fname in os.listdir(folder)
+                 if fname.endswith('.txt')]
+    filenames = sorted(filenames)
+    
+    for fname in filenames: 
+        if fname in res_content and not args.recompute:
+            print(f'Scores already computed for {fname}')
+            continue
+            
+        print(f'Computing PARENT and BLEU scores for {fname}')
+        
+        with open(fname, mode='r', encoding='utf8') as f:
+            candidates = [line.strip().split() for line in f if line.strip()]
+            
+        BLEU = corpus_bleu(list_of_references, candidates)
+        PARENT = corpus_parent(candidates, list_of_references, tables)
+        
+        res = {
+            'BLEU': BLEU,
+            'PARENT-precision': np.mean(PARENT[0]),
+            'PARENT-recall': np.mean(PARENT[1]),
+            'PARENT-f1': np.mean(PARENT[2])
+        }
+        
+        print(res)
+        
+        # We dump the dict each time to be more robust against interruption
+        with open(res_filename, mode="w", encoding="utf8") as resfile:
+            json.dump(res_content, resfile)
